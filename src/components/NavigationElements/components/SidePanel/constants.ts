68d37f169d2725c7a2f56478ddbea98ef36acb18
kevin B <k.bidet@cityscoot.eu>
2024-07-29 11:27:42 +0200

    feat: ✨ artist / song nav link in sidebar

--- src/components/NavigationElements/components/SidePanel/constants.ts
+++ src/components/NavigationElements/components/SidePanel/constants.ts
@@ -20,6 +20,8 @@ export const navigation: navigationItem[] = [
   { name: 'Accueil', href: '#', icon: HomeIcon },
   { name: 'Recherche', href: '#', icon: MagnifyingGlassIcon },
   { name: 'Calendrier', href: '#', icon: CalendarIcon },
+  { name: 'Toutes nos musiques', href: '/tracks', icon: CalendarIcon },
+  { name: 'Tous nos artists', href: '/artists', icon: CalendarIcon },
 ];
 
 export const playlistList: playlist[] = [
