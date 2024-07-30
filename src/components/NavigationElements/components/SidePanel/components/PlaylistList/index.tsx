bb7275e0a026c28e4162ca92765f51467660202d
kevin B <k.bidet@cityscoot.eu>
2024-07-30 11:19:44 +0200

    feat: ✨ display playlist and allow to create some

--- src/components/NavigationElements/components/SidePanel/components/PlaylistList/index.tsx
+++ src/components/NavigationElements/components/SidePanel/components/PlaylistList/index.tsx
@@ -1,22 +1,54 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import PlaylistListItem from '~/components/NavigationElements/components/SidePanel/components/PlaylistList/PlaylistListItem';
-import { playlist } from '~/components/NavigationElements/components/SidePanel/constants';
+import CreatePlaylistButton from '~/components/NavigationElements/components/SidePanel/components/PlaylistList/CreatePlaylistButton';
+import {
+  createPlaylist,
+  getPlaylists,
+  Playlist,
+} from '~/utils/function/localStorage';
+import { v4 as uuidv4 } from 'uuid';
 
 interface PlaylistListProps {
   title: string;
-  list: playlist[];
 }
 
-const PlaylistList = ({ title, list }: PlaylistListProps) => {
+const PlaylistList = ({ title }: PlaylistListProps) => {
+  const [playlistList, setPlaylistList] = useState<Playlist[]>([]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const playlists = getPlaylists();
+      if (!playlists.length) {
+        createPlaylist('Likes', uuidv4());
+      }
+      setPlaylistList(playlists);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleStorageChanges = () => {
+      const playlists = getPlaylists();
+      setPlaylistList(playlists);
+    };
+
+    window.addEventListener('storage', handleStorageChanges);
+    return () => window.removeEventListener('storage', handleStorageChanges);
+  }, []);
+
   return (
     <>
-      <div className={'text-xs font-semibold leading-6 text-gray-400'}>
+      <div className={'text-xl font-semibold leading-6 text-gray-400'}>
         {title}
       </div>
       <ul role={'list'} className={'-mx-2 mt-2 space-y-1'}>
-        {list.map((playlist: playlist) => (
-          <li key={playlist.name}>
-            <PlaylistListItem playlist={playlist} />
+        <li>
+          <CreatePlaylistButton />
+        </li>
+        {playlistList.map(({ title, tracks, id }) => (
+          <li key={title}>
+            <PlaylistListItem title={title} tracks={tracks} id={id} />
           </li>
         ))}
       </ul>
