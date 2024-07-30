3b8018d3ed5e335b65af04268a5d395adebde1ab
kevin B <k.bidet@cityscoot.eu>
2024-07-30 11:15:59 +0200

    chg: 💄 sticky audio player

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -28,21 +28,29 @@ const AudioPlayer = () => {
   }
 
   return (
-    <div className={'bg-gray-900'}>
-      <div className={'my-3 flex h-12 w-full items-center justify-center'}>
-        <ReactPlayer
-          controls
-          url={currentTrack?.preview_url}
-          width={'100%'}
-          playing={playing}
-          height={'100%'}
-          volume={0.05} // todo: to remove
-          config={{
-            file: {
-              forceAudio: true,
-            },
-          }}
-        />
+    <div className={'pb-32'}>
+      <div
+        className={
+          'fixed bottom-0 z-50 w-full bg-gray-900 lg:left-[288px] lg:right-0 lg:w-auto '
+        }
+      >
+        <div
+          className={'my-3 h-12 w-full items-center justify-center lg:block'}
+        >
+          <ReactPlayer
+            controls
+            url={currentTrack?.preview_url}
+            width={'100%'}
+            playing={playing}
+            height={'100%'}
+            volume={0.05} // todo: to remove
+            config={{
+              file: {
+                forceAudio: true,
+              },
+            }}
+          />
+        </div>
       </div>
     </div>
   );
