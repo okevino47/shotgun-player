73d34b390241d070a77bdda6f310405d91c2b2f2
kevin B <k.bidet@cityscoot.eu>
2024-07-30 22:56:28 +0200

    feat: 🌐 Locales added

--- src/core/locales/en.ts
+++ src/core/locales/en.ts
@@ -1,5 +1,40 @@
 export default {
-  'hello': 'Hello',
-  'hello.world': 'Hello world!',
-  'welcome': 'Hello {name}!'
-} as const
+  common: {
+    seeAll: 'See all',
+    topTracks: 'Top Tracks',
+    topArtists: 'Top Artists',
+    ariaLikeButton: 'Like or unlike song',
+  },
+  error: {
+    artistsFetch: 'Error fetching all artists',
+  },
+  navigationElements: {
+    navigation: {
+      home: 'Home',
+      allTracks: 'All our musics',
+      allArtists: 'All our artists',
+    },
+    playlistSectionTitle: 'Playlists',
+    addPlaylist: 'Create new playlist',
+  },
+  tracks: {
+    allMusic: 'All our musics',
+    ariaPlayButton: 'Play song',
+  },
+  playlist: {
+    deletePlaylist: 'Delete Playlist',
+    emptyPlaylist: 'This playlist is empty',
+    playlistTitle: 'Playlist',
+    albumAltImage: 'album image',
+    emptyPlaylistMessage1: 'Oups, This playlist is empty !',
+    emptyPlaylistMessage2: "N'hésitez pas à la remplir depuis ",
+    emptyPlaylistMessage3: 'our track list',
+  },
+  modal: {
+    createPlaylist: {
+      title: 'Name of the playlist',
+      cancelButton: 'Cancel',
+      createButton: 'Confirmer',
+    },
+  },
+} as const;
--- src/core/locales/fr.ts
+++ src/core/locales/fr.ts
@@ -1,5 +1,40 @@
 export default {
-  'hello': 'Bonjour',
-  'hello.world': 'Bonjour tout le monde!',
-  'welcome': 'Coucou {name}!'
-} as const
+  common: {
+    seeAll: 'Voir tout',
+    topTracks: 'Meilleures chansons',
+    topArtists: 'Meilleurs artistes',
+    ariaLikeButton: 'Aimer ou ne pas aimer la chanson',
+  },
+  error: {
+    artistsFetch: 'Erreur lors de la récupération de tous les artistes',
+  },
+  navigationElements: {
+    navigation: {
+      home: 'Accueil',
+      allTracks: 'Toutes nos musiques',
+      allArtists: 'Tous nos artistes',
+    },
+    playlistSectionTitle: 'Playlists',
+    addPlaylist: 'Créer une nouvelle playlist',
+  },
+  tracks: {
+    allMusic: 'Toutes nos musiques',
+    ariaPlayButton: 'Lire la chanson',
+  },
+  playlist: {
+    deletePlaylist: 'Supprimer la playlist',
+    emptyPlaylist: 'Cette playlist est vide',
+    playlistTitle: 'Playlist',
+    albumAltImage: "image de l'album",
+    emptyPlaylistMessage1: 'Oups, cette playlist est vide !',
+    emptyPlaylistMessage2: "N'hésitez pas à la remplir depuis ",
+    emptyPlaylistMessage3: 'notre liste de chansons',
+  },
+  modal: {
+    createPlaylist: {
+      title: 'Nom de la playlist',
+      cancelButton: 'Annuler',
+      createButton: 'Confirmer',
+    },
+  },
+} as const;
