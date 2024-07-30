97d2ff426d935b6af4ab2b64120b8779f0dc914b
kevin B <k.bidet@cityscoot.eu>
2024-07-30 15:38:54 +0200

    chg: ♻️ z-index on elispsis menu

--- src/components/buttons/EllipsisTrackButton.tsx
+++ src/components/buttons/EllipsisTrackButton.tsx
@@ -33,7 +33,7 @@ const EllipsisTrackButton = ({
       <MenuItems
         transition
         className={
-          'absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
+          'absolute right-0 z-30 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
         }
       >
         {playlistList
